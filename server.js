51422c3fa3ccbae29f1e7dac2195d1eb629c10f9
brownieeedev <barnacsordas@gmail.com>
2023-09-27 20:23:54 +0200

    Added server and a router

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ dotenv.config({ path: "./config.env" });
 
 const app = require("./app");
 
-//Start backend server on port 5000
-const port = process.env.PORT || 5000;
+//Start backend server on port 4000
+const port = process.env.PORT || 4000;
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
 });
